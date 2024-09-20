5f47f3705bb34cbc88e98288555c06a47f1cebff
baccno <perin.pontel@gmail.com>
2024-09-19 23:34:45 -0300

    Alteração do ultimo arquivo, colocando a versão final para a entrega para a Codex

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
     },
-    specPattern: 'cypress/e2e/features/**/*.cy.{js,jsx,ts,tsx}', // Verifique se o padrão está correto
-    supportFile: 'cypress/support/e2e.js', // Especifique o caminho do arquivo de suporte
+    specPattern: 'cypress/e2e/features/**/*.cy.{js,jsx,ts,tsx}', // Verifique se o padrão está corretos
   },
 });
